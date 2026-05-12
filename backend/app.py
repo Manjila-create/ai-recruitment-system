b2ea71a81f7ee385c664a6b8ddc71ef4ede248bd
Manjila <Mangila.adhikari111@gmail.com>
2026-05-13 01:20:08 +0545

    Made a static home page

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Backend Running"
+def login():
+    return render_template('login.html')
+
+@app.route('/user')
+def user():
+    return render_template('user.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
